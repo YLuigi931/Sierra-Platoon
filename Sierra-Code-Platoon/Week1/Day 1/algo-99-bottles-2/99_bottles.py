8a7bb00fca1c6a7c4ae06522cf57c006362c0372
LuigiY <Product4conker@Gmail.com>
2022-10-05 13:07:57 -0500

    Python version solve for bottles

--- Sierra-Code-Platoon/Week1/Day 1/algo-99-bottles-2/99_bottles.py
+++ Sierra-Code-Platoon/Week1/Day 1/algo-99-bottles-2/99_bottles.py
@@ -1,5 +1,6 @@
 def bottle_song():
 	# write your code here!
-
+	for x in range(100, 0, -1):
+		print(f"{x} bottles of beer on the wall, {x} bottles of beer. Take one down and pass it around,")
 
 bottle_song()
