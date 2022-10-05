8671121e961167efa926c75d4e89b1ed34e44ccc
LuigiY <Product4conker@Gmail.com>
2022-10-05 12:41:05 -0500

    Hopefully it goes to the right address

--- Sierra-Code-Platoon/Week1/Day 1/algo-99-bottles-2/99_bottles.js
+++ Sierra-Code-Platoon/Week1/Day 1/algo-99-bottles-2/99_bottles.js
@@ -1,5 +1,11 @@
 function bottleSong() {
   // code goes here
+  for(let x = 100; x > 0; x--){
+
+    console.log(x + " bottles of beer on the wall, " + x 
+    + " bottles of beer. Take one down and pass it around, ");
+
+  }
 };
 
 bottleSong();
