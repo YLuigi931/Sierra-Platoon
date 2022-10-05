3cc8ef32381aab1c8d813aaf2e908328fb82ab0b
LuigiY <Product4conker@Gmail.com>
2022-10-05 14:33:38 -0500

    Fibonacci Iterative Python version

--- Sierra-Code-Platoon/Week1/Day 2/algo-fibonacci/python/fibonacci.py
+++ Sierra-Code-Platoon/Week1/Day 2/algo-fibonacci/python/fibonacci.py
@@ -1,4 +1,14 @@
 def fibonacci(n):
-  pass
-
-
+  current = 0
+  holder = 0 
+  previous = 1
+  
+  while(n != 0):
+    holder = current
+    current += previous
+    previous = holder
+    n -= 1
+  
+  return current
+  
+#print(fibonacci(7))
