c75010124f57fb5548477f54d2ec593d8c1a9b82
LuigiY <Product4conker@Gmail.com>
2022-10-05 13:22:21 -0500

    Update for python linear search

--- Sierra-Code-Platoon/Week1/Day 1/algo-linear-search/python/linear_search.py
+++ Sierra-Code-Platoon/Week1/Day 1/algo-linear-search/python/linear_search.py
@@ -1,11 +1,28 @@
+from array import array
+
+
 array_to_search_through = []
 for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
     # your code here
-    return 0
+    
+    for x in range(len(array_to_search_through)):
+        if(array_to_search_through[x] == value_to_find):
+            return x
+    
+    return None
 
 def linear_search_global(value_to_find, array_to_search_through):
     # your code here
-    return [0]
+    result = []
+    
+    for x in range(len(array_to_search_through)):
+        if(array_to_search_through[x] == value_to_find):
+            result.append(x)
+    
+    if(len(result) == 0):
+        return None
+    
+    return result
