48a3cf8ddb7e0bc38abb6f09b49e607f6d774a60
LuigiY <Product4conker@Gmail.com>
2022-10-09 15:12:12 -0500

    Day 4 update

--- Sierra-Code-Platoon/Week1/Day 4/algo-armstrong-numbers-main/python/armstrong_numbers.py
+++ Sierra-Code-Platoon/Week1/Day 4/algo-armstrong-numbers-main/python/armstrong_numbers.py
@@ -2,22 +2,20 @@
 import math
 
 def find_armstrong_numbers(numbers):
-    results =[]
-    # string_num = str(numbers)
-    # total = 0
+    results = []
+    
     for item in numbers:
         holder = str(item)
         if len(holder) == 1:
             results.append(item)
-            pass
+            
         elif len(holder) == 2:
             temp1 = int(holder[0])
             temp2 = int(holder[1])
             summ = math.pow(temp1, 2) + math.pow(temp2, 2)
             if summ == item:
                 results.append(item)
-            #instructions
-            pass
+            
         elif len(holder) == 3:
             tem1 = int(holder[0])
             tem2 = int(holder[1])
@@ -26,10 +24,7 @@ def find_armstrong_numbers(numbers):
             if sum3 == item:
                 results.append(item)
     return results
-    # results = []
-    # comparison = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407]
-    # for num in numbers:
-    #     print(num)
-    #     if num in comparison:
-    #         results.append(num)
-    # return results
+
+# use list comprehension to shorten
+# lambda
+# functools.reduce
