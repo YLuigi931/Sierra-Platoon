9689569fa047a0f6616723ff674f0f5a3492ad5a
LuigiY <Product4conker@Gmail.com>
2022-10-09 16:48:27 -0500

    Update again of day 5 folder

--- Sierra-Code-Platoon/Week1/Day 5/algo-calculate-mode-main/python/calculate_mode.py
+++ Sierra-Code-Platoon/Week1/Day 5/algo-calculate-mode-main/python/calculate_mode.py
@@ -1,2 +1,26 @@
-def calculate_mode():
-    pass
+'''
+find the most common element
+
+'''
+
+def calculate_mode(lst):
+    tally = {}
+    result = []
+    for item in lst:
+        if item not in tally:
+            tally[item] = 1
+        else:
+            tally[item] += 1
+
+    current_max = max(tally.values())
+    # print(current_max)
+    for num in tally:
+        # print(tally.get(num))
+        if tally.get(num) == current_max:
+            result.append(num)
+
+    # print(result)
+    return result
+    
+# calculate_mode([1,2,3,3])
+
