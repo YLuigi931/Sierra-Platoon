a9d335db1b1eba81489542a4d68cb10f623b2864
LuigiY <Product4conker@Gmail.com>
2022-10-09 15:48:24 -0500

    update pad

--- Sierra-Code-Platoon/Week1/Day 5/algo-pad-array-main/python/pad_array.py
+++ Sierra-Code-Platoon/Week1/Day 5/algo-pad-array-main/python/pad_array.py
@@ -1,3 +1,28 @@
 #REMEMBER TO PSEUDOCODE
+''' 
+part 1:
+check wether the length of the current array is longer
+than the min_size if not return the original list
+
+part 2:
+if the value == None and the min_size is longer than the list.
+the list will append 'None' base on the length of the min_size
+
+part 3:
+If the min_size is over the list size and there is a 3rd value.
+instead of appending 'None', append what that value
+
+'''
+
 def pad(array, min_size, value = None):
-    pass 
+    lst_size = len(array)
+    arr_copy = array
+    if(min_size < lst_size):
+        return array
+    else:
+        for item in range(lst_size-1,min_size-1):
+            arr_copy.append(value)
+        return arr_copy
+    
+# print(pad([1,2,3], 5, 'apple'))
+# print(pad([1,2,3], 5))
