60c1b7b0cb25d393524c21ba32b555ec06a87583
LuigiY <Product4conker@Gmail.com>
2022-10-09 15:11:37 -0500

    Day 5 update

--- Sierra-Code-Platoon/Week1/Day 5/algo-character-count-main/python/char_count.py
+++ Sierra-Code-Platoon/Week1/Day 5/algo-character-count-main/python/char_count.py
@@ -1,2 +1,28 @@
+import numpy as np
+from operator import itemgetter
+
 def char_count(str):
   # Your code here
+  temp = str.replace(" ", "")
+  container = {}
+  for letter in temp:
+    if letter not in container:
+      container[letter] = 1
+    else:
+      container[letter] += 1  
+  
+  lst1 = container.items()
+  res = [[i for i, j in lst1],[j for i, j in lst1]]
+  temp1 = np.array(list(zip(res[0], res[1])))
+  temp2 = sorted(temp1.tolist(), key=itemgetter(0), reverse=False)
+  mr = np.array(temp2)
+  temp3 = sorted(mr.tolist(), key=itemgetter(1), reverse=True)
+  
+  for bag in temp3:
+    holder = bag[1]
+    bag[1] = int(holder)
+    
+  return temp3
+  # print(temp3)
+  
+# char_count("an apple a day will keep the doctor away")
--- Sierra-Code-Platoon/Week1/Day 5/algo-palindromes-main/python/palindrome.py
+++ Sierra-Code-Platoon/Week1/Day 5/algo-palindromes-main/python/palindrome.py
@@ -1,2 +1,13 @@
+from pkg_resources import empty_provider
+import re
+
 def palindrome(word):
-    # Write code here
+   
+    temp_word1 = str(word).lower().replace(" ", "")
+    out = re.sub("[,-.]","", temp_word1)
+    
+    return out == out[::-1]
+    
+    
+    
+# print(palindrome("NOva is here"))
