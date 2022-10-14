fc65e72871d31ff2599a039a544dcdb5d9d02fbd
LuigiY <Product4conker@Gmail.com>
2022-10-14 08:19:27 -0500

    update the folders

--- Sierra-Code-Platoon/Week2/Day3/oop-boggle-i-master/boggle_board.py
+++ Sierra-Code-Platoon/Week2/Day3/oop-boggle-i-master/boggle_board.py
@@ -1,6 +1,27 @@
+import random
+import string
+
 class BoggleBoard:
   
   def __init__(self):
+    self.blank = BoggleBoard.blank(self)
   
-
   def shake(self):
+    # print(''.join(random.choices(string.ascii_uppercase, k=4)))
+    lst = []
+    for item in range(4):
+      random_letters = ''.join(random.choices(string.ascii_uppercase, k=4))
+      lst.append(random_letters)
+    
+    print(lst)
+    
+    
+    # for index in range(4):
+      
+  def blank(self):
+    blank = '```python\n   ____\n   ____\n   ____\n   ____\n```'
+    print(blank)
+    
+    
+test1 = BoggleBoard()
+test1.shake()
