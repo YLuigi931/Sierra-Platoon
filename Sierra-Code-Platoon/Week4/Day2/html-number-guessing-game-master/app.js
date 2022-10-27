40d90f4765344c8d0fa23204e7e821ae2dc264e9
LuigiY <Product4conker@Gmail.com>
2022-10-27 08:14:24 -0500

    updating current folders 10/27

---
+++ Sierra-Code-Platoon/Week4/Day2/html-number-guessing-game-master/app.js
@@ -0,0 +1,10 @@
+console.log("HELLO PAPA PLATOON!")
+// Your function(s) should go here that will interact with the webpage or DOM
+
+function msg() {
+    alert("Hello world!");
+}
+
+function randomNumber() {
+    return Math.floor(Math.random() * 10);
+}
