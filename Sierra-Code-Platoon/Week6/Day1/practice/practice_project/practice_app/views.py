ee4295bced87cdfe7c2a5d7602dbf695adc9e1d8
LuigiY <Product4conker@Gmail.com>
2022-11-08 09:58:39 -0600

    update Nov8

--- Sierra-Code-Platoon/Week6/Day1/practice/practice_project/practice_app/views.py
+++ Sierra-Code-Platoon/Week6/Day1/practice/practice_project/practice_app/views.py
@@ -1,6 +1,48 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from . models import Students
+from django.http import JsonResponse
+import json
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    # # play with this code block
+    # student = Students(first_name ='johnny', last_name ='irwin', email = 'irwin@Gmail')
+    # student.save()
+    # # it adds to the database
+    # return render(request, 'index.html')
+    
+    # this code block will view the objects 
+    # students = Students.objects.all()
+    # print(students[1].first_name)
+    # return render(request, 'index.html', {'students': students})
+
+    #student will be deleted
+    # student_to_be_deleted = Students.objects.get(id=2)
+    # print(student_to_be_deleted)
+    # student_to_be_deleted.delete()
+    
+    students = Students.objects.all()
+    
+    return render(request, 'index.html', {'students': students})
+
+@csrf_exempt
+def add_student(request):
+    print(request.method)
+    # check for post (this is the reciever)
+    if request.method == 'POST':
+        print('recieved')
+        # when we recieve the request we can parse the data and take the data in body
+        body = json.loads(request.body)
+        print(body)
+        
+        # parse the body data to make the student object
+        new_student = Students(first_name = body['first'], last_name = body['last'], email = body['email'])
+        # adds to the database
+        new_student.save()
+        # this will give an error if new_student is added here flat due to its a object with parts
+        # try json.loads(new_student)
+        # another solution would be making a dictionry and appending 
+        # look up jamgo serializer
+    return JsonResponse({'new_student': new_student.first_name})
+    
--- Sierra-Code-Platoon/Week6/Day1/practice/practice_project/static/main.js
+++ Sierra-Code-Platoon/Week6/Day1/practice/practice_project/static/main.js
@@ -1,2 +1,19 @@
 console.log('HI there')
 
+function add_student(event){
+    // don't forget to add .value
+    const first_name = document.getElementById('first_name').value
+    const last_name = document.getElementById('last_name').value
+    const email = document.getElementById('email').value
+
+    // sender pass the info to back end. look at views.py
+    // this makes a request to the backend to add this info
+    axios
+        .post('student/', {'first': first_name, 'last': last_name, 'email': email})
+        .then( (response) => {
+                console.log(response)
+            }
+        )
+
+}
+
