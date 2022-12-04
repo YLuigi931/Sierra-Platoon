53ce2f933e8f523e8412d7cc2175d6cf4f9983d5
LuigiY <Product4conker@Gmail.com>
2022-12-04 14:07:54 -0600

    Update Day5 folder

--- Sierra-Code-Platoon/Week9/Day5/class-project/client/src/App.jsx
+++ Sierra-Code-Platoon/Week9/Day5/class-project/client/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+import reactLogo from './assets/react.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from 'react-bootstrap/NavBar'
+import Nav from 'react-bootstrap/Nav';
+import axios from 'axios';
+import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import CategoriesPage from './components/CategoriesPage';
+import HomePage from './components/HomePage';
+import PostsPage from './components/PostsPage';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <div className="App">
-      <p>Hi there</p>
+      <h1> World of Kittens </h1>
+
+      <NavBar>
+        <Nav.Link href='/'> Home </Nav.Link>
+        <Nav.Link href='/categories'> Categories </Nav.Link>
+        <Nav.Link href='/posts'> Posts </Nav.Link>
+      </NavBar>
+
+      <Router>
+        <Routes>
+          <Route path='' element={<HomePage/>}/>
+          <Route path='categories/' element={<CategoriesPage/>}/>
+          <Route path='posts/' element={<PostsPage/>}/>
+        </Routes>
+      </Router>
+
     </div>
   )
 }
